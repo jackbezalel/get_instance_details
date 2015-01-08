5bfd71fe0e9276da669ae936911e1e3cd17960ee
Jack Bezalel <ybezalel@gmail.com>
2015-01-08 11:03:21 +0200

    Support now for Solaris 8 to 11 and Redhat

--- README.md
+++ README.md
@@ -8,3 +8,38 @@ This tool will acquire important hardware and operating system details of an ins
 Details could include hardware or virtual hardware attributes, as well as operating system or firmware attributes.
 
 For now this tool will focus on Unix and Linux variants Operating Systems only.
+
+Supported Platforms:
+
+1. Solaris 8,9,10,11, including LDOMs and all kinds of Zones on all hardware types
+2. Redhat Linux on all Hardware types
+
+How to use osinfo.sh:
+
+Login to the system as root
+mount OSINFO tool directory via NFS
+cd to the osinfo directory
+./osinfo.sh
+
+This tool will NOT show virtual network cards – it is aimed to point to physical devices only.
+On Solaris zones it will not allow operation since it takes all its resources from the host machine.
+
+Sample run on Solaris:
+
+Vendor=SunOS
+Major Version=5.11
+ZONE global
+Machine sol11svr OSINFO will be reported at /patches/machines/OSINFO/sol11svr/201501061915
+CPU:        8
+MEM: Memory size: 4096 Megabytes
+DISK: 0. c0t5000CCA03C3BB5B4d0 <HITACHI-H106030SDSUN300G-A2B0-279.40GB>
+DISK: 1. c0t5000CCA03C3BB8E0d0 <HITACHI-H106030SDSUN300G-A2B0-279.40GB>
+DISK: 2. c0t6005076D02810005400000000000002Ed0 <IBM-2145-0000-1.00TB>
+LAN: LINK                CLASS     MTU    STATE    OVER
+LAN: net1                phys      1500   unknown  --
+LAN: net4                phys      1500   up       --
+LAN: net3                phys      1500   unknown  --
+LAN: net0                phys      1500   up       --
+LAN: net2                phys      1500   unknown  --
+LAN: net6                phys      1500   up       --
+
---
+++ osinfo.sh
@@ -0,0 +1,136 @@
+#!/bin/sh
+
+# Author: Jack Bezalel
+
+#Functions
+
+# General notes
+#
+# The following situations NOT considered an error, thus 
+# we exit with $TRUE (all OK) in those cases:
+# 1. Not supporting an operating system 
+# 2. No patch for this operating system
+#
+
+FALSE=1
+TRUE=0
+
+OSINFO_ROOT="/patches/machines/OSINFO"
+
+. ./services.sh
+. ./get_linux_vendor.sh
+. ./get_os_major_vers.sh
+
+#Main
+
+HOSTNAME="`hostname`"
+OSNAME="`uname -s`"
+OSINFO_DATE="`date +"%Y%m%d%H%M"`"
+
+case $OSNAME in
+Linux)
+	LINUX_VENDOR="`GET_LINUX_VENDOR`"
+	if 	[ $LINUX_VENDOR = "error" ];
+	then
+        	echo "We do not support OSINFO for $OSNAME yet.."
+        	exit $TRUE
+	fi
+	
+	echo "Vendor=$LINUX_VENDOR"
+
+	OS_MAJOR_VERS="`GET_OS_MAJOR_VERS $LINUX_VENDOR`"
+
+	if 	[ $OS_MAJOR_VERS = "error" ];
+	then
+        	echo "We do not support OSINFO for $LINUX_VENDOR yet.."
+        	exit $TRUE
+	fi
+	
+	echo "Major Version=$OS_MAJOR_VERS"
+	OSARCH="`uname -i`"
+	MACHINE_OSINFO_DIR="$OSINFO_ROOT/$HOSTNAME/$OSINFO_DATE"
+	echo "Machine $HOSTNAME OSINFO will be reported at $MACHINE_OSINFO_DIR"
+
+	if [ $LINUX_VENDOR = "redhat" ];
+	then
+		mkdir -p $MACHINE_OSINFO_DIR
+		echo "CPU: `cat /proc/cpuinfo | grep processor | wc -l`" \
+               		>> $MACHINE_OSINFO_DIR/osinfo.log
+        	echo "MEM: `cat /proc/meminfo | grep -i memtotal`" \
+                	>> $MACHINE_OSINFO_DIR/osinfo.log
+        	parted -l | grep "Disk /dev" | grep -v "mapper" | \
+                while read i; do   echo -e "DISK: $i" ; done \
+                	        >> $MACHINE_OSINFO_DIR/osinfo.log
+        	lspci | grep -i 'Ethernet controller' | grep -vi vmware | \
+                	while read i; do   echo -e "LAN: $i" ; done \
+                        	>> $MACHINE_OSINFO_DIR/osinfo.log
+        	cat $MACHINE_OSINFO_DIR/osinfo.log
+	else
+		echo "We do not support OSINFO for $LINUX_VENDOR yet.."
+		exit $TRUE
+	fi
+;;
+SunOS)
+	OS_MAJOR_VERS="`GET_OS_MAJOR_VERS $OSNAME`"
+
+	if 	[ $OS_MAJOR_VERS = "error" ];
+	then
+        	echo "We do not support OSINFO for $OSNAME yet.."
+        	exit $TRUE
+	fi
+
+	echo "Vendor=$OSNAME"	
+	echo "Major Version=$OS_MAJOR_VERS"
+	
+	# Check if this machine has zones software installed
+	# Then if that's a sparse zone it should be handled by
+	# updating the global zone rather than directly
+
+	if	[ -f /usr/bin/zonename ];
+	then
+		ZONE_NAME="`zonename`"
+		echo "ZONE $ZONE_NAME"
+		ZONE_TYPE="`pkgcond -n is_what | grep 'is_global_zone=0'`"
+		if	[ " $ZONE_TYPE" != " " ];
+		then	
+			echo "Execution aborted since this NOT a global zone."
+			echo "Please re-run on the global zone."
+			exit $TRUE
+		fi
+	fi
+
+	OSARCH="`uname -m`"
+
+        MACHINE_OSINFO_DIR="$OSINFO_ROOT/$HOSTNAME/$OSINFO_DATE"
+        echo "Machine $HOSTNAME OSINFO will be reported at $MACHINE_OSINFO_DIR"
+
+        mkdir -p $MACHINE_OSINFO_DIR
+	echo "CPU: `psrinfo | wc -l`" \
+              >> $MACHINE_OSINFO_DIR/osinfo.log
+        echo "MEM: `prtconf | grep -i 'Memory size'`" \
+              >> $MACHINE_OSINFO_DIR/osinfo.log
+        echo "0 q" | format | \
+		i=" "
+		echo "0 q" | format 2>/dev/null | \
+		until [ "`(echo $i | cut -c0-12)`" = "Specify disk" ]
+		do
+        		read i
+        		DISK_LINE=" `echo $i | grep '^[0-9].'`"
+			if 	[ "$DISK_LINE" != " " ];
+			then	
+				echo "DISK:" $DISK_LINE
+			fi
+		done \
+                	>> $MACHINE_OSINFO_DIR/osinfo.log
+                dladm show-link | egrep -vi "vnic|vnet" | \
+                        while read i; do   echo "LAN: $i" ; done \
+                                >> $MACHINE_OSINFO_DIR/osinfo.log
+                cat $MACHINE_OSINFO_DIR/osinfo.log
+
+;;
+*)
+	echo "We do not support OSINFO for $OSNAME yet.."
+	exit $TRUE
+;;
+esac
+
