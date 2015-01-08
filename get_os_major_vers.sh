da3b8dcc76cbaa04678d6f68324bf2242bb8604d
Jack Bezalel <ybezalel@gmail.com>
2015-01-08 13:01:04 +0200

    Set get_os_major_vers.sh to use case instead of if

--- get_os_major_vers.sh
+++ get_os_major_vers.sh
@@ -5,21 +5,28 @@ GET_OS_MAJOR_VERS()
 
 OS_VENDOR=$1
 
-if [ "$OS_VENDOR" = "redhat" ];
-then
-        OS_MAJOR_VERS="`cat /etc/redhat-release | head -1 | awk '{ print $7 }' | awk -F. '{ print $1 }'`"
+case $OS_VENDOR in
+redhat)
+        OS_MAJOR_VERS="`cat /etc/redhat-release | head -1 | \
+		awk '{ print $7 }' | awk -F. '{ print $1 }'`"
         echo $OS_MAJOR_VERS
         return $TRUE
-fi
+;;
 
-if [ "$OS_VENDOR" = "SunOS" ];
-then
+SunOS)
         OS_MAJOR_VERS="`uname -r`"
         echo $OS_MAJOR_VERS
         return $TRUE
-fi
+;;
 
-echo "error"
+*)
+	echo "error"
+        exit $TRUE
+;;
+
+esac
+
+
 return $FALSE
 
 }
